a95083ed908adef2ed3382e932816f34496d17ef
SeulLip <nils.viklund.s@gmail.com>
2025-06-14 10:21:23 +0200

    Revert and Jump function
    Reverted my InputBuffer script and try to make a jump buffer but I only got to making the jump() get called and actually but it doesn't buffer nor show up in my inputdisplay.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,7 +6,6 @@ var current_input_combo : StringName = ""
 
 const SPEED = 300.0
 const dash_tap_interval = 0.25
-const JUMP_VELOCITY = -400.0
 @export var jump_height : float
 @export var jump_time_to_peak : float
 @export var jump_time_to_decent : float
@@ -29,146 +28,149 @@ func _physics_process(delta):
 	# Add the gravity.
 	velocity.y += gets_gravity() * delta
 		
-	if not isAttacking:
+	#if not isAttacking:
 		
 	# Handle jump.
-		if Input.is_action_just_pressed("up") and is_on_floor():
-			velocity.y = jump_velocity
+		#if Input.is_action_just_pressed("move_up") and is_on_floor():
+			#velocity.y = jump_velocity
 
-		var direction := Input.get_axis("left", "right") 
-		if direction:
-			if direction > 0:
-				rotation_degrees = 0
-				scale.y = 1
-			else:
-				rotation_degrees = 180
-				scale.y = -1
-			
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-		if Input.is_action_just_pressed("left"): 
-			if Input.is_action_just_pressed("left"): 
-				if Time.get_ticks_msec() / 1000.0 - last_left_tap_time < dash_tap_interval:
-					isDashing = true
-				else:
-					isDashing = false
-					
-				last_left_tap_time = Time.get_ticks_msec() / 1000.0
-		if Input.is_action_just_pressed("right"): 
-			if Input.is_action_just_pressed("right"): 
-				if Time.get_ticks_msec() / 1000.0 - last_right_tap_time < dash_tap_interval:
-					isDashing = true
-				else:
-					isDashing = false
-					
-				last_right_tap_time = Time.get_ticks_msec() / 1000.0
-		if isDashing:
-			velocity.x += SPEED * 25 * direction
-			isDashing = false
-		if not isAttacking: 
-				$Weapon/AnimationPlayer.play("Idle");
-	else:
-		velocity = Vector2.ZERO
-	if last_input_time + 0.5 < Time.get_ticks_msec() / 1000.0:
-		current_input_combo = ""
-		current_input_sequence = []
-	var input_changed = false
-	if current_input_combo == "":
-		
-		if Input.is_action_just_pressed("down"):
-			current_input_combo = ("down")
-			input_changed = true
-		elif Input.is_action_just_pressed("up"):
-			current_input_combo = ("up")
-			input_changed = true
-		elif Input.is_action_just_pressed("left"):
-			current_input_combo = ("left")
-			input_changed = true
-		elif Input.is_action_just_pressed("right"):
-			current_input_combo = ("right")
-			input_changed = true
-		elif Input.is_action_just_pressed("light attack"):
-			current_input_combo = ("light attack")
-			input_changed = true
-		elif Input.is_action_just_pressed("heavy attack"):
-			current_input_combo = ("heavy attack")
-			input_changed = true
-	else:
-		if Input.is_action_just_released(current_input_combo):
-			current_input_sequence.append(current_input_combo)
-			current_input_combo = ""
-			input_changed = true
-		else:
-			if Input.is_action_just_pressed("down"):
-				input_changed = true
-				match current_input_combo:
-					"left":
-						current_input_sequence.append("down-left")
-						current_input_combo = ""
-					"right":
-						current_input_sequence.append("down-right")
-						current_input_combo = ""
-					_:
-						current_input_sequence.append(current_input_combo)
-						current_input_combo = "down"
-			elif Input.is_action_just_pressed("up"):
-				input_changed = true
-				match current_input_combo:
-					"left":
-						current_input_sequence.append("up-left")
-						current_input_combo = ""
-					"right":
-						current_input_sequence.append("up-right")
-						current_input_combo = ""
-					_:
-						current_input_sequence.append(current_input_combo)
-						current_input_combo = "up"
-			elif Input.is_action_just_pressed("left"):
-				input_changed = true
-				match current_input_combo:
-					"down":
-						current_input_sequence.append("down-left")
-						current_input_combo = ""
-					"up":
-						current_input_sequence.append("up-left")
-						current_input_combo = ""
-					_:
-						current_input_sequence.append(current_input_combo)
-						current_input_combo = "up"
-			elif Input.is_action_just_pressed("right"):
-				input_changed = true
-				match current_input_combo:
-					"down":
-						current_input_sequence.append("down-right")
-						current_input_combo = ""
-					"up":
-						current_input_sequence.append("up-right")
-						current_input_combo = ""
-					_:
-						current_input_sequence.append(current_input_combo)
-						current_input_combo = "right"
-			elif Input.is_action_just_pressed("light attack"):
-				input_changed = true
-				current_input_sequence.append(current_input_combo)
-				current_input_combo = "light attack"
-			elif Input.is_action_just_pressed("heavy attack"):
-				input_changed = true
-				current_input_sequence.append(current_input_combo)
-				current_input_combo = "heavy attack"
-	if input_changed:
-		last_input_time = Time.get_ticks_msec() / 1000.0
-		for combo in combos: 
-			if current_input_sequence == combo.input:
-				$Weapon/AnimationPlayer.play(combo.attack.animation);
-				isAttacking = true
-				current_input_combo = ""
-				current_input_sequence = []
-	print("current_input_combo", current_input_combo) 
-	print("current_input_sequence", current_input_sequence)
+		#var direction := Input.get_axis("left", "right") 
+		#if direction:
+			#if direction > 0:
+				#rotation_degrees = 0
+				#scale.y = 1
+			#else:
+				#rotation_degrees = 180
+				#scale.y = -1
+			#
+			#velocity.x = direction * SPEED
+		#else:
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#if Input.is_action_just_pressed("left"): 
+			#if Input.is_action_just_pressed("left"): 
+				#if Time.get_ticks_msec() / 1000.0 - last_left_tap_time < dash_tap_interval:
+					#isDashing = true
+				#else:
+					#isDashing = false
+					#
+				#last_left_tap_time = Time.get_ticks_msec() / 1000.0
+		#if Input.is_action_just_pressed("right"): 
+			#if Input.is_action_just_pressed("right"): 
+				#if Time.get_ticks_msec() / 1000.0 - last_right_tap_time < dash_tap_interval:
+					#isDashing = true
+				#else:
+					#isDashing = false
+					#
+				#last_right_tap_time = Time.get_ticks_msec() / 1000.0
+		#if isDashing:
+			#velocity.x += SPEED * 25 * direction
+			#isDashing = false
+		#if not isAttacking: 
+				#$Weapon/AnimationPlayer.play("Idle");
+	#else:
+		#velocity = Vector2.ZERO
+	#if last_input_time + 0.5 < Time.get_ticks_msec() / 1000.0:
+		#current_input_combo = ""
+		#current_input_sequence = []
+	#var input_changed = false
+	#if current_input_combo == "":
+		#
+		#if Input.is_action_just_pressed("down"):
+			#current_input_combo = ("down")
+			#input_changed = true
+		#elif Input.is_action_just_pressed("up"):
+			#current_input_combo = ("up")
+			#input_changed = true
+		#elif Input.is_action_just_pressed("left"):
+			#current_input_combo = ("left")
+			#input_changed = true
+		#elif Input.is_action_just_pressed("right"):
+			#current_input_combo = ("right")
+			#input_changed = true
+		#elif Input.is_action_just_pressed("light attack"):
+			#current_input_combo = ("light attack")
+			#input_changed = true
+		#elif Input.is_action_just_pressed("heavy attack"):
+			#current_input_combo = ("heavy attack")
+			#input_changed = true
+	#else:
+		#if Input.is_action_just_released(current_input_combo):
+			#current_input_sequence.append(current_input_combo)
+			#current_input_combo = ""
+			#input_changed = true
+		#else:
+			#if Input.is_action_just_pressed("down"):
+				#input_changed = true
+				#match current_input_combo:
+					#"left":
+						#current_input_sequence.append("down-left")
+						#current_input_combo = ""
+					#"right":
+						#current_input_sequence.append("down-right")
+						#current_input_combo = ""
+					#_:
+						#current_input_sequence.append(current_input_combo)
+						#current_input_combo = "down"
+			#elif Input.is_action_just_pressed("up"):
+				#input_changed = true
+				#match current_input_combo:
+					#"left":
+						#current_input_sequence.append("up-left")
+						#current_input_combo = ""
+					#"right":
+						#current_input_sequence.append("up-right")
+						#current_input_combo = ""
+					#_:
+						#current_input_sequence.append(current_input_combo)
+						#current_input_combo = "up"
+			#elif Input.is_action_just_pressed("left"):
+				#input_changed = true
+				#match current_input_combo:
+					#"down":
+						#current_input_sequence.append("down-left")
+						#current_input_combo = ""
+					#"up":
+						#current_input_sequence.append("up-left")
+						#current_input_combo = ""
+					#_:
+						#current_input_sequence.append(current_input_combo)
+						#current_input_combo = "up"
+			#elif Input.is_action_just_pressed("right"):
+				#input_changed = true
+				#match current_input_combo:
+					#"down":
+						#current_input_sequence.append("down-right")
+						#current_input_combo = ""
+					#"up":
+						#current_input_sequence.append("up-right")
+						#current_input_combo = ""
+					#_:
+						#current_input_sequence.append(current_input_combo)
+						#current_input_combo = "right"
+			#elif Input.is_action_just_pressed("light attack"):
+				#input_changed = true
+				#current_input_sequence.append(current_input_combo)
+				#current_input_combo = "light attack"
+			#elif Input.is_action_just_pressed("heavy attack"):
+				#input_changed = true
+				#current_input_sequence.append(current_input_combo)
+				#current_input_combo = "heavy attack"
+	#if input_changed:
+		#last_input_time = Time.get_ticks_msec() / 1000.0
+		#for combo in combos: 
+			#if current_input_sequence == combo.input:
+				#$Weapon/AnimationPlayer.play(combo.attack.animation);
+				#isAttacking = true
+				#current_input_combo = ""
+				#current_input_sequence = []
+	#print("current_input_combo", current_input_combo) 
+	#print("current_input_sequence", current_input_sequence)
 
 	move_and_slide()
-	
+func jump():
+		if is_on_floor():
+			velocity.y = jump_velocity
+
 func gets_gravity() -> float:
 	return jump_gravity if velocity.y < 0.0 else fall_gravity
 
